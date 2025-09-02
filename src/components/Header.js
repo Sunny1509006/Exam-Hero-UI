6add0125877807bbd02810577ddf25c06f806ddd
BS01414 <sunny1509006@gmail.com>
2025-09-02 12:19:58 +0600

    programs has been added on the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,6 +13,23 @@ const Header = () => {
           <ul className="nav-list">
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
+            <li className="dropdown">
+              <span className="dropdown-toggle">Programs</span>
+              <div className="dropdown-menu">
+                <div className="dropdown-section">
+                  <h4>SSC Programs</h4>
+                  <Link to="/ssc-science">SSC Science</Link>
+                  <Link to="/ssc-humanities">SSC Humanities</Link>
+                  <Link to="/ssc-business">SSC Business Studies</Link>
+                </div>
+                <div className="dropdown-section">
+                  <h4>HSC Programs</h4>
+                  <Link to="/hsc-science">HSC Science</Link>
+                  <Link to="/hsc-humanities">HSC Humanities</Link>
+                  <Link to="/hsc-business">HSC Business Studies</Link>
+                </div>
+              </div>
+            </li>
             <li><Link to="/founders-journey">Founder's Journey</Link></li>
             <li><Link to="/faq">FAQ</Link></li>
             <li><Link to="/contact">Contact</Link></li>
